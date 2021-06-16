79041466b6c418ecc407820d981c21d37a7a31d7
ebintobin <ebin1234578@gmail.com>
2021-06-16 17:32:02 -0400

    game scene

---
+++ Pong 5-5/Assets/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float extraSpeedPerHit;
+    public float maxExtraSpeed;
+
+    int hitCounter = 0;
+
+    void Start()
+    {
+        StartCoroutine(this.StartBall());
+    }
+
+    public IEnumerator StartBall(bool isStartingPlayer1 = true) {
+
+        this.hitCounter = 0;
+        yield return new WaitForSeconds(2);
+        if (isStartingPlayer1) {
+            this.MoveBall(new Vector2(-1, 0));
+        } else {
+            this.MoveBall(new Vector2(1, 0));
+        }
+        
+    }
+
+    public void MoveBall(Vector2 dir)
+    {
+        dir = dir.normalized;
+
+        float speed = this.movementSpeed + this.hitCounter * this.extraSpeedPerHit;
+
+        Rigidbody2D rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+
+        rigidbody2D.velocity = dir * speed * Time.deltaTime;
+    }
+}
