9411b7ec2bd1038a1d3151040010b3cf14719444
ebintobin <ebin1234578@gmail.com>
2021-06-16 18:58:29 -0400

    6/16

---
+++ Pong 5-5/Assets/Assets/Scripts/RacketPlayer1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketPlayer1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movementSpeed;
+
+    private void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+    }
+}
